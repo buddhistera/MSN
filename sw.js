d6df25706f643f048fa4a161042ec4a3c92b5338
buddhistera <santhananda86@gmail.com>
2026-04-30 13:57:09 +0530

    Add service worker for caching assets
    Implement service worker caching for offline access.

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+const cacheName = 'v1';
+const cacheAssets = [
+  'index.html',
+  'style.css',
+  'script.js'
+];
+
+// ගොනු ගබඩා කිරීම (Install)
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      cache.addAll(cacheAssets);
+    })
+  );
+});
+
+// Offline අවස්ථාවේදී ගොනු ලබා දීම (Fetch)
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
 
